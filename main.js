fecf5bf348e5f99e2979ee626f7144dfb4327648
roxzera <SrzRoxOri@gmail.com>
2024-02-10 14:31:07 -0300

    js

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
 $(document).ready(function (){
-    $('button-submit').on('submit', function(e) {
+    if (grecaptcha.getResponse() != '') return true;
+
+    alert('aaa')
+
+    $('form').on('submit', function(e) {
         e.preventDefault();
+
+        $('#cpf').val('')
+        $('#cep').val('')
+        $('#phone').val('')
+    })
+
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '425.321.534-23'
+    })
+    $('#cep').mask('00000-000', {
+        placeholder: '42342-289'
+    })
+    $('#phone').mask('(00) 00000-0000', {
+        placeholder: '(11) 99144-4324'
     })
 })
